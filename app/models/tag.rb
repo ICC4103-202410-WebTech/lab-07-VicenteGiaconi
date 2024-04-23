b9458bb6587bb66eacbf20743cf647e7ab2e354e
VicenteGiaconi <vgiaconi@miuandes.cl>
2024-04-23 18:24:12 -0400

    feat: adding missing validation on tag.rb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,5 @@ class Tag < ApplicationRecord
     has_many :post_tags, class_name: 'PostTag'
     has_many :posts, through: :post_tags, class_name: 'Post'
 
-    validates :name, uniqueness: { message: "already exists" }
+    validates :name, presence: { message: "must be provided by the user" }, uniqueness: { message: "already exists" }
 end
