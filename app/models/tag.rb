28c6c5529af6340f1387a478c97e8e0126f78453
VicenteGiaconi <vgiaconi@miuandes.cl>
2024-04-26 16:53:47 -0400

    feat: queries showrcuts implemented on user and tags model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,7 @@
 class Tag < ApplicationRecord
     has_many :post_tags, class_name: 'PostTag'
     has_many :posts, through: :post_tags, class_name: 'Post'
+    has_many :users, through: :posts, class_name: 'User'
 
     validates :name, presence: { message: "must be provided by the user" }, uniqueness: { message: "already exists" }
 end
